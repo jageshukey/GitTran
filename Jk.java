c55123c1ef944493c3c6c7c75cab8c2969e19d05
jageshukey <121861353+jageshukey@users.noreply.github.com>
2023-01-10 16:51:57 +0530

    adding agai

--- Jk.java
+++ Jk.java
@@ -1,6 +1,23 @@
+import java.util.*;
+
 public class Jk {
     public static void main(String args[])
     {
-        
+    
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=5;k>=i;k--)
+            {
+                System.out.print("X");
+            }
+            System.out.println();
+        }
+
+
+
     }
 }
